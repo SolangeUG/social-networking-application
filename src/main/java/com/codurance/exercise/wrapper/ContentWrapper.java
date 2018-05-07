cf965b8727de157f39a27d72e18719e7b9d2a961
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-08 01:04:08 +0200

    Implement string representations of a content wrapper information

--- src/main/java/com/codurance/exercise/wrapper/ContentWrapper.java
+++ src/main/java/com/codurance/exercise/wrapper/ContentWrapper.java
@@ -1,6 +1,9 @@
 package com.codurance.exercise.wrapper;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * A wrapper class to hold content information.
@@ -10,6 +13,7 @@ public class ContentWrapper {
     private final String owner;
     private final String content;
     private final LocalDate creationDate;
+    private ResourceBundle messageBundle;
 
     /**
      * Constructor
@@ -21,6 +25,8 @@ public class ContentWrapper {
         this.owner = owner;
         this.content = message;
         this.creationDate = timestamp;
+
+        this.messageBundle = ResourceBundle.getBundle("messages", Locale.getDefault());
     }
 
     /**
@@ -47,4 +53,86 @@ public class ContentWrapper {
         return this.creationDate;
     }
 
+    /**
+     * Returns a string representation of the object. In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return owner + " -> " + formatContent();
+    }
+
+    /**
+     * Return a string representation of this wrapper's content
+     * @return wrapper content with duration
+     */
+    public String formatContent() {
+        return content + " " + formatDuration();
+    }
+
+    /**
+     * Return a string representation of the time difference
+     * between this content creation date and the time at which this
+     * method is called.
+     * @return time difference as string
+     */
+    private String formatDuration() {
+        Duration timeDiff = Duration.between(LocalDate.now(), creationDate);
+        String result;
+
+        long duration = timeDiff.toDays();
+        if (duration >= 1) {
+            result = formatDurationWithUnit("day", duration);
+        } else {
+            duration = timeDiff.toHours();
+            if (duration >= 1) {
+                result = formatDurationWithUnit("hour", duration);
+            } else {
+                duration = timeDiff.toMinutes();
+                if (duration >= 1) {
+                    result = formatDurationWithUnit("minute", duration);
+                } else {
+                    duration = timeDiff.toMillis() * 1000;
+                    result = formatDurationWithUnit("hour", duration);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Return a localized string representation of a duration
+     * @param unit duration unit
+     * @param duration duration amount
+     * @return localized string representation of duration
+     */
+    private String formatDurationWithUnit(String unit, long duration) {
+        String messageKey = "content.time.difference" + unit;
+        String result = messageBundle.getString(messageKey);
+        if (duration > 1) {
+            messageKey += "s";
+            result = String.format(
+                    messageBundle.getString(messageKey),
+                    duration
+            );
+        }
+        return result;
+    }
 }
