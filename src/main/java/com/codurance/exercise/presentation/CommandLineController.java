b3500e4c722113de34a5ea6ee8eea4ed4b792bc2
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-07 16:23:08 +0200

    Define UI controller

---
+++ src/main/java/com/codurance/exercise/presentation/CommandLineController.java
@@ -0,0 +1,64 @@
+package com.codurance.exercise.presentation;
+
+import com.codurance.exercise.application.SocialNetwork;
+import com.codurance.exercise.application.SocialNetworkImpl;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Command-Line Controller
+ * @author Solange U. Gasengayire
+ */
+public class CommandLineController {
+    private final SocialNetwork network;
+
+    /**
+     * Default constructor
+     */
+    public CommandLineController() {
+        super();
+        this.network = new SocialNetworkImpl();
+    }
+
+    /**
+     * Post a message from the user
+     * @param username specified user name
+     * @param message the message to be posted
+     * @param timestamp when the message is posted
+     */
+    public void postMessage(String username, String message, LocalDate timestamp) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    /**
+     * Follow a user
+     * @param username specified user name
+     * @param subject user to follow
+     * @param since when the subscription is started
+     */
+    public void addFollowing(String username, String subject, LocalDate since) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    /**
+     * Return a list of a given user content
+     * @param user specified user name
+     * @return all created content by the user
+     */
+    public List<String> getUserMessages(String user) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    /**
+     * Return a list of a given user content
+     * and content from the users they follow
+     * @param user specified user
+     * @return all content from the user and the users they follow
+     */
+    public List<String> getAllMessages(String user) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+
+}
