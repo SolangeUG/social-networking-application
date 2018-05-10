33675560a1264240a8d9dbd1c45f31abdcd77c8b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-10 21:33:28 +0200

    Simplify main class add move operations execution responsibility to controller

--- src/main/java/com/codurance/exercise/Application.java
+++ src/main/java/com/codurance/exercise/Application.java
@@ -1,29 +1,27 @@
 package com.codurance.exercise;
 
-import com.codurance.exercise.presentation.Command;
 import com.codurance.exercise.presentation.CommandLineController;
 import com.codurance.exercise.util.Constants;
-import com.codurance.exercise.wrapper.ContentWrapper;
 
 import java.io.Console;
-import java.time.LocalDateTime;
-import java.util.Map;
 
 /**
  * Console-based social networking application
  * @author Solange U. Gasengayire
  */
 public class Application {
-    private static final CommandLineController controller
-            = new CommandLineController();
-
-    private static Console console = System.console();
 
     /**
      * Application entry point
      * @param args application arguments
      */
     public static void main( String[] args ) {
+        // initialize application controller
+        CommandLineController controller = new CommandLineController();
+
+        // initialize application UI
+        Console console = System.console();
+
         if (console == null) {
             System.err.println(Constants.CONSOLE_ERROR_MESSAGE);
             System.exit(1);
@@ -37,97 +35,22 @@ public class Application {
         String instruction = console.readLine(prompt);
 
         while (instruction != null && ! instruction.isEmpty()) {
+
             // execute user instruction
-            executeCommand(instruction);
+            String result = controller.executeInstruction(instruction);
+            if ("QUIT".equals(result)) {
+                console.writer().println(Constants.EXIT_MESSAGE);
+                console.writer().flush();
+                console.writer().close();
+                System.exit(0);
+            } else if(! result.isEmpty()) {
+                console.writer().println(result);
+                console.writer().flush();
+            }
+
             // read next command
             instruction = console.readLine("> ");
         }
     }
 
-    /**
-     * Execute input command from the user
-     * @param instruction user command (string)
-     */
-    private static void executeCommand(String instruction) {
-
-        Command command = new Command(instruction);
-        String operator = command.getOperator();
-        String operand = command.getOperand();
-
-        switch (command.getOperation()) {
-            case POST:
-                post(operator, operand);
-                break;
-
-            case READ:
-                read(operator);
-                break;
-
-            case FOLLOW:
-                follow(operator, operand);
-                break;
-
-            case WALL:
-                displayWall(operator);
-                break;
-
-            case QUIT:
-                console.writer().println(Constants.EXIT_MESSAGE);
-                console.writer().flush();
-                console.writer().close();
-                System.exit(0);
-
-            default:
-                break;
-        }
-    }
-
-    /**
-     * Message post feature
-     * @param user name of the user initiating the action
-     * @param message message to be posted
-     */
-    private static void post(String user, String message) {
-        controller.postMessage(user, message, LocalDateTime.now());
-    }
-
-    /**
-     * Following feature
-     * @param user name of the user initiating the action
-     * @param subscription name of the user they're following
-     */
-    private static void follow(String user, String subscription) {
-        controller.addFollowing(user, subscription, LocalDateTime.now());
-    }
-
-    /**
-     * Display a given user's timeline
-     * @param user name of the user initiating the action
-     */
-    private static void read(String user) {
-        Map<LocalDateTime, ContentWrapper> userMessages = controller.getUserMessages(user);
-        StringBuilder builder = new StringBuilder();
-        for (ContentWrapper wrapper: userMessages.values()) {
-            builder.append(wrapper.getFormattedContent());
-            builder.append("\n");
-        }
-        console.writer().println(builder.toString());
-        console.writer().flush();
-    }
-
-    /**
-     * Display a given user's timeline, including
-     * timelines of all their subscriptions
-     * @param user name of the user initiating the action
-     */
-    private static void displayWall(String user) {
-        Map<LocalDateTime, ContentWrapper> wall = controller.getAllMessages(user);
-        StringBuilder builder = new StringBuilder();
-        for (ContentWrapper wrapper: wall.values()) {
-            builder.append(wrapper.toString());
-            builder.append("\n");
-        }
-        console.writer().println(builder.toString());
-        console.writer().flush();
-    }
 }
--- src/main/java/com/codurance/exercise/presentation/CommandLineController.java
+++ src/main/java/com/codurance/exercise/presentation/CommandLineController.java
@@ -24,12 +24,52 @@ public class CommandLineController {
     }
 
     /**
+     * Execute input command from the user
+     * @param instruction user command (as astring)
+     * @return result of the command
+     */
+    public String executeInstruction(String instruction) {
+        Command command = new Command(instruction);
+        String operator = command.getOperator();
+        String operand = command.getOperand();
+
+        String result = "";
+
+        switch (command.getOperation()) {
+            case POST:
+                postMessage(operator, operand, LocalDateTime.now());
+                break;
+
+            case READ:
+                result = getContentAsString(getUserMessages(operator), false);
+                break;
+
+            case FOLLOW:
+                addFollowing(operator, operand, LocalDateTime.now());
+                break;
+
+            case WALL:
+                result = getContentAsString(getAllMessages(operator), true);
+                break;
+
+            case QUIT:
+                result = "QUIT";
+                break;
+
+            default:
+                break;
+        }
+
+        return result;
+    }
+
+    /**
      * Post a message from the user
      * @param username specified user name
      * @param message the message to be posted
      * @param timestamp when the message is posted
      */
-    public void postMessage(String username, String message, LocalDateTime timestamp) {
+    void postMessage(String username, String message, LocalDateTime timestamp) {
         ContentWrapper content = new ContentWrapper(username, message, timestamp);
         this.network.createContent(content);
     }
@@ -40,7 +80,7 @@ public class CommandLineController {
      * @param subject user to follow
      * @param since when the subscription is started
      */
-    public void addFollowing(String username, String subject, LocalDateTime since) {
+    void addFollowing(String username, String subject, LocalDateTime since) {
         SubscriptionWrapper subscription = new SubscriptionWrapper(username, subject, since);
         this.network.createSubscription(subscription);
     }
@@ -50,7 +90,7 @@ public class CommandLineController {
      * @param user specified user name
      * @return all created content by the user
      */
-    public Map<LocalDateTime, ContentWrapper> getUserMessages(String user) {
+    Map<LocalDateTime, ContentWrapper> getUserMessages(String user) {
         return this.network.getContent(user);
     }
 
@@ -60,8 +100,27 @@ public class CommandLineController {
      * @param user specified user
      * @return all content from the user and the users they follow
      */
-    public Map<LocalDateTime, ContentWrapper> getAllMessages(String user) {
+    Map<LocalDateTime, ContentWrapper> getAllMessages(String user) {
         return this.network.getAllContent(user);
+    }
+
+    /**
+     * Return requested content as a string
+     * @param content requested content
+     * @param withUser flag to include (or not) the user's name
+     * @return a string representing the content
+     */
+    private String getContentAsString(Map<LocalDateTime, ContentWrapper> content, boolean withUser) {
+        StringBuilder builder = new StringBuilder();
+        for (ContentWrapper wrapper: content.values()) {
+            if (withUser) {
+                builder.append(wrapper.toString());
+            } else {
+                builder.append(wrapper.getFormattedContent());
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
     }
 
 }
