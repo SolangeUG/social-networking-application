99fd21e42053d9309ab08d9aaf4ae3a01698dece
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-11 15:43:41 +0200

    Adapt model implementation in accordance with API definition

--- src/main/java/com/codurance/exercise/application/SocialNetworkImpl.java
+++ src/main/java/com/codurance/exercise/application/SocialNetworkImpl.java
@@ -4,8 +4,6 @@ import com.codurance.exercise.domain.Connection;
 import com.codurance.exercise.domain.Content;
 import com.codurance.exercise.domain.Message;
 import com.codurance.exercise.domain.User;
-import com.codurance.exercise.wrapper.ContentWrapper;
-import com.codurance.exercise.wrapper.SubscriptionWrapper;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -19,12 +17,13 @@ public class SocialNetworkImpl implements SocialNetwork {
 
     /**
      * Create/post content
-     * @param content content information to be created
+     * @param username  name of the user who posts the content
+     * @param content   actual content to be posted
+     * @param timestamp date and time when the content is created
      */
     @Override
-    public void createContent(ContentWrapper content) {
-        if (content != null) {
-            String username = content.getOwner();
+    public void createContent(String username, String content, LocalDateTime timestamp) {
+        if (content != null && !content.isEmpty()) {
             User user = users.get(username);
 
             // new (unknown) user
@@ -34,29 +33,28 @@ public class SocialNetworkImpl implements SocialNetwork {
             }
 
             // save user's message
-            Message message = new Message(user, content.getContent(), content.getCreationTimestamp());
+            Message message = new Message(user, content, timestamp);
             user.addContent(message);
         }
     }
 
     /**
      * Create/add subscription (following)
+     * @param username     name of the user who posts the content
      * @param subscription subscription information to be created
+     * @param timestamp    date and time when the content is created
      */
     @Override
-    public void createSubscription(SubscriptionWrapper subscription) {
-        if (subscription != null) {
-            String username = subscription.getSubscriptionOwner();
+    public void createSubscription(String username, String subscription, LocalDateTime timestamp) {
+        if (subscription != null && !subscription.isEmpty()) {
 
             // assume that only "known" users can follow other users
             User user = users.get(username);
             if (user != null) {
-                String other = subscription.getSubject();
-
                 // assume that a user can only follow a "known" user
-                User target = users.get(other);
+                User target = users.get(subscription);
                 if (target != null) {
-                    Connection link = new Connection(user, target, subscription.getSubscriptionTimestamp());
+                    Connection link = new Connection(user, target, timestamp);
                     user.addConnection(link);
                 }
             }
@@ -64,26 +62,36 @@ public class SocialNetworkImpl implements SocialNetwork {
     }
 
     /**
+     * Get created content for a given user as a string
+     * @param username specified user name
+     * @return a string representing the list of content
+     */
+    @Override
+    public String getContent(String username) {
+        User user = users.get(username);
+        return getMapAsString(user.getAllContent(), false);
+    }
+
+    /**
+     * Get an aggregated list of content from a given user as a string
+     * and the users they are following (they subscribed to)
+     * @param username the specified user name
+     * @return a string representing the list of aggregated content
+     */
+    @Override
+    public String getAllContent(String username) {
+        TreeMap<LocalDateTime, Content> allContent = getUserAggregatedContent(username);
+        return getMapAsString(allContent,true);
+    }
+
+    /**
      * Get created content for a given user
      * @param username specified user name
      * @return the request list of content
      */
-    @Override
-    public Map<LocalDateTime, ContentWrapper> getContent(String username) {
-        Map<LocalDateTime, ContentWrapper> userContent = new TreeMap<>(Comparator.reverseOrder());
-
+    Map<LocalDateTime, Content> getUserContent(String username) {
         User user = users.get(username);
-        if (user != null) {
-            for (Content content: user.getAllContent()) {
-                Message message = (Message) content;
-                LocalDateTime timestamp = message.getCreationDate();
-                ContentWrapper wrapper = new ContentWrapper(
-                        user.getName(), message.getContent(), timestamp);
-                userContent.put(timestamp, wrapper);
-            }
-        }
-
-        return userContent;
+        return user.getAllContent();
     }
 
     /**
@@ -92,9 +100,8 @@ public class SocialNetworkImpl implements SocialNetwork {
      * @param username the specified user name
      * @return the request map of aggregated content
      */
-    @Override
-    public Map<LocalDateTime, ContentWrapper> getAllContent(String username) {
-        TreeMap<LocalDateTime, ContentWrapper> allContent = new TreeMap<>(Comparator.reverseOrder());
+    TreeMap<LocalDateTime, Content> getUserAggregatedContent(String username) {
+        TreeMap<LocalDateTime, Content> allContent = new TreeMap<>(Comparator.reverseOrder());
 
         User requestedUser = users.get(username);
         if (requestedUser != null) {
@@ -102,13 +109,28 @@ public class SocialNetworkImpl implements SocialNetwork {
             List<User> following = requestedUser.getSubscriptions();
             following.add(requestedUser);
 
-            // return an aggregated list of the requested user's messages
+            // build an aggregated list of the requested user's messages
             // and all the messages of the users they follow
             for (User user: following) {
-                allContent.putAll(getContent(user.getName()));
+                allContent.putAll(user.getAllContent());
             }
         }
-
         return allContent;
+    }
+
+    /**
+     * Return a string representing the content of a map
+     * @param map the specified map of content
+     * @param withUser include user name
+     * @return the map as a string
+     */
+    private String getMapAsString(Map<LocalDateTime, Content> map, boolean withUser) {
+        StringBuilder builder = new StringBuilder();
+        for (Content content: map.values()) {
+            String contentStr = withUser ? content.toString() : content.formatContent();
+            builder.append(contentStr);
+            builder.append("\n");
+        }
+        return builder.toString();
     }
 }
