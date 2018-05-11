c98bb28a0acf8b554d6ce25ec00c10f44f811ed9
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-11 20:50:20 +0200

    Code cleanup

--- src/test/java/com/codurance/exercise/presentation/CommandLineControllerTest.java
+++ src/test/java/com/codurance/exercise/presentation/CommandLineControllerTest.java
@@ -3,8 +3,6 @@ package com.codurance.exercise.presentation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
